923d99d64bb02a6fabbb0af0bdc9b44221a97827
Sunny <sunnylee945@hotmail.com>
2017-08-16 20:47:57 +0800

    added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811134539) do
-
+ActiveRecord::Schema.define(version: 20170816124541) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +39,13 @@ ActiveRecord::Schema.define(version: 20170811134539) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
