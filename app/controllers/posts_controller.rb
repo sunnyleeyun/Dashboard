08173c85e4f78582c529f016c200279dcd698f0a
Sunny <sunnylee945@hotmail.com>
2017-08-16 21:41:07 +0800

    added category checkbox collection input into new post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
     private
     
     def post_params
-        params.require(:post).permit(:title, :content)
+        params.require(:post).permit(:title, :content, category_ids: [])
     end
 
 end
